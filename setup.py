5ff1fd8fb6cca4f861713aff0d5761f2a2efc43f
Kiss Péter <kiss.peter@linuxadm.hu>
2024-07-31 15:52:06 +0200

    drop 3.6

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setup_options = dict(
         "Topic :: System :: Distributed Computing",
         "Natural Language :: English",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
